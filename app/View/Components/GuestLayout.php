6733799371dc86a2185c16a5e700092b7078b6f4
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-08-16 14:17:28 -0500

    Update GuestLayout.php

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -8,7 +8,7 @@ use Illuminate\View\View;
 class GuestLayout extends Component
 {
     /**
-     * Get the view
+     * Get the view and contents that represents the component.
      */
     public function render(): View
     {
